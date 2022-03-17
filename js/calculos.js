4bb797e536f2cffce8ccb96d06c8287f61437528
anadiaza <adabelleira@gmail.com>
2022-03-17 11:05:30 +0100

    actualizacion

--- js/calculos.js
+++ js/calculos.js
@@ -1,3 +1,6 @@
+// Definir variables
+var n = 0;
+
 // Funcion para leer numeros Reales
 function leerNumero() {
     num = parseInt(
@@ -6,14 +9,41 @@ function leerNumero() {
     return num;
 }
 
-function mostrarResultados() {
-    document.getElementById('td1').innerHTML = n;
-    document.getElementById('td2').innerHTML = factorial();
+function mostrarResultados(nf) {
+    document.getElementById('td21').innerHTML = n;
+    document.getElementById('td22').innerHTML = factorial(nf);
+}
+function mostrarFactorial(nf) {
+    document.getElementById('td21').innerHTML = nf;
+    document.getElementById('td22').innerHTML = factorial(nf);
+}
+function mostrarMultiplos(n, m) {
+    document.getElementById("td11").innerHTML = n;
+    document.getElementById('td12').innerHTML = m;
+    document.getElementById('td13').innerHTML = multiplosDehasta(n, m);
 }
 
-function factorial() {
+function multiplosDehasta (n, m){
+     mensaje = " los multiplos de "
+     + n +
+     "hasta " + m +
+     "son: ";
+
+     multiplo = 0;
+
+     for (i=1; multiplo < m; i++) {
+         console.log(i, "", multiplo);
+
+         /*multiplos = " " + n*i;*/
+         multiplo = n*i;
+         mensaje = mensaje + multiplo + " ";
+
+     }
+     return mensaje;
+}
+function factorial(nf) {
     var factorial = 1;
-    for ( i=1; i <= n; i++ ) {
+    for ( i=1; i <= nf; i++ ) {
         factorial = factorial * i;
     }
 
